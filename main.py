e7354e2c47f35e4d05a884cc0e9751f3f956c3ed
Max Solovyev <sys.dll@gmail.com>
2017-11-14 17:03:43 +0300

    tmp

--- main.py
+++ main.py
@@ -15,9 +15,9 @@ def parseAddress(input):
         return input
 
 
-def retrieveWebPage(ADDRESS):
+def retrieveWebPage(addr):
         try:
-                web_handle = urllib2.urlopen(ADDRESS)
+                web_handle = urllib2.urlopen(addr)
         except urllib2.HTTPError, e:
                 error_desc = BaseHTTPServer.BaseHTTPRequestHandler.responses[e.code][0]
                 print "Cannot retrieve URL: HTTP Error Code", e.code
@@ -44,9 +44,9 @@ def mailsend():
     smtp.sendmail(FROM_FIELD, TO_ADDRESS, msg)
     smtp.quit()
 
-website_handle = retrieveWebPage(ADDRESS)
-website_text = website_handle.read()
-matches = sre.findall(WORD, website_text)
+# website_handle = retrieveWebPage(ADDRESS)
+# website_text = website_handle.read()
+# matches = sre.findall(WORD, website_text)
 
 #DEBUG
 # if matches:
@@ -54,12 +54,42 @@ matches = sre.findall(WORD, website_text)
 # else:
 #     print MSG_IF_NOT_MATCH
 
-finish = False
+# finish = False
 
-while not finish:
-    if matches:
-        # print MSG_IF_MATCH
-        mailsend()
-        finish = True
-    else:
-        sleep(DELAY_CHECK)
+# while not finish:
+#     if matches:
+#         # print MSG_IF_MATCH
+#         mailsend()
+#         finish = True
+#     else:
+#         sleep(DELAY_CHECK)
+
+
+class LostFilm():
+    def __init__(self, url, name, season, ep):
+        self.url = url
+        self.name = name
+        self.season = season
+        self.ep = ep
+        print "url " + self.url
+        print "Сериал: " + self.name
+        print "Сезон: " + str(self.season)
+        print "Ждем серию: " + str(self.ep)
+
+    def _check(self):
+        website_handle = retrieveWebPage(self.url)
+        website_text = website_handle.read()
+        result = "/%s/season_%d/episode_%d" % (self.name, self.season, self.ep)
+        matches = sre.findall(result, website_text)
+        return matches
+
+    def go(self):
+        res = _check()
+        if res:
+            self.ep=+1
+            mailsend()
+            print 'send'
+
+ST = LostFilm('http://lostfilm.tv','Stranger_Things', 2,6)
+ST._check()
+ST.go()
